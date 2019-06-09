99b7d85927e37d706a40c5cbc1f793038cbb4e49
harshithaselvam27 <50586653+harshithaselvam27@users.noreply.github.com>
2019-06-09 12:32:06 +0530

    Create text_wrap.py

---
+++ text_wrap.py
@@ -0,0 +1,16 @@
+def wrap(string, max_width):
+    a=textwrap.fill(string,max_width)
+
+
+
+    return a
+
+#solution 2
+def wrap(string, max_width):
+    a=textwrap.wrap(string,max_width)
+    for i in a:
+        print(i)
+
+
+
+    return " "
